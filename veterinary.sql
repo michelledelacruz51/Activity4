be2be38ab3e5b6cb89094a888d7cca75082046ea
Michelle De La Cruz <24-30521@g.batstate-u.edu.ph>
2025-11-05 17:40:52 +0800

    feat: create table owners

---
+++ veterinary.sql
@@ -0,0 +1,10 @@
+
+
+CREATE TABLE owners(
+    ownerid INT PRIMARY KEY,
+    ofirstname VARCHAR(50),
+    olastname VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
+);
